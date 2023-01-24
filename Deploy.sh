27168d4fe227c5c43112122d42b960b371a0f01c
Arjun <arjun@infracloud.io>
2023-01-24 19:14:03 +0530

    fixed the issue

--- Deploy.sh
+++ Deploy.sh
@@ -1,14 +1,27 @@
+echo "Installing Postgrws main cluster"
+
+kubectl apply -k ./
+
+echo -e "\n"
+
+sleep 3 
+
+kubectl wait   --for=condition=ready pod  -l name=postgres-operator --timeout=200s -n postgres
+
+kubectl wait   --for=condition=ready pod  -l cluster-name=acid-main-cluster   --timeout=200s -n postgres
+
+echo -e "Installing standby cluster\n"
+
+
+kubectl apply -f postgres-operator/standby-manifest.yaml
+
+
 POSTGRES_PASS=`kubectl get secrets -n postgres postgres.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
-
-
-echo -e "postgresql password: `echo $POSTGRES_PASS | base64 -d` \n"
-
-
 
 STANDBY_PASS=`kubectl get secrets -n postgres standby.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
 
 
-echo -e "standby password: `echo $STANDBY_PASS  | base64 -d`\n"
+# echo -e "standby password: `echo $STANDBY_PASS  | base64 -d`\n"
 
 
 
@@ -26,11 +39,31 @@ echo -e "Patching the standby cluster with main cluster secret"
 kubectl patch secret standby.acid-standby-cluster.credentials.postgresql.acid.zalan.do --type='json' -p='[{"op" : "replace" ,"path" : "/data/password" ,"value" : "'${STANDBY_PASS}'"}]'
 
 
+
+kubectl  logs acid-standby-cluster-0 | grep "FATAL:  password authentication failed" > /dev/null
+
+if [ $? = 0 ]; then
+echo "Pod is not running need a restart"
+
+kubectl delete pod acid-standby-cluster-0
+
+
+else 
+echo "Successfully patched the secrete on standby cluster"
+fi 
+
+
+echo -e "postgresql password: `echo $POSTGRES_PASS | base64 -d` \n"
+
 echo -e "\n"
 
-echo  -e "forwarding port to local \n "
-pc
+# echo  -e "forwarding port to local \n "
+# pc
 
-kubectl  port-forward acid-main-cluster-0 5432:5432 &
+# echo -e "ACID MAIN"
+# kubectl  port-forward acid-main-cluster-0 5434:5432    & 
 
-kubectl  port-forward acid-standby-cluster-0 5433:5432
+
+# echo -e "ACID STANDBY"
+# kubectl  port-forward acid-standby-cluster-0 5435:5432 
+
