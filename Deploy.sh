ef8c244a27edb7bf1c61335bf0fe0f42812e32ec
Arjun <arjun@infracloud.io>
2023-01-25 23:31:38 +0530

    fixed the issue

--- Deploy.sh
+++ Deploy.sh
@@ -1,69 +1,125 @@
-echo "Installing Postgrws main cluster"
+#!/bin/bash 
+
+wait()
+
+{
+    sleep $1
+
+}
+
+secret_patch(){
+
+   secret_count=`kubectl  get secret  -n postgres| grep postgresql.acid.zalan.do  | wc -l`
+
+   if [ ! $secret_count = '6' ]; then
+      echo "secrets are creating......! wait for a while"
+      wait 8
+   fi
+
+   POSTGRES_PASS=`kubectl get secrets -n postgres postgres.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
+
+   STANDBY_PASS=`kubectl get secrets -n postgres standby.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
+
+   # echo -e "standby password: `echo $STANDBY_PASS  | base64 -d`\n"
+
+   echo -e "Patching the standby cluster with main cluster secret...................................!"
+
+   kubectl patch secret postgres.acid-standby-cluster.credentials.postgresql.acid.zalan.do --type='json' -p='[{"op" : "replace" ,"path" : "/data/password" ,"value" : "'${POSTGRES_PASS}'"}]'
+
+   echo -e "\n"
+
+   echo -e "Patching the standby cluster with main cluster secret....................................!"
+
+   kubectl patch secret standby.acid-standby-cluster.credentials.postgresql.acid.zalan.do --type='json' -p='[{"op" : "replace" ,"path" : "/data/password" ,"value" : "'${STANDBY_PASS}'"}]'
+
+
+}
+
+show_pass(){
+   
+   echo -e "\n"
+
+   POSTGRES_PASS=`kubectl get secrets -n postgres postgres.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
+
+   echo -e "postgresql password: `echo $POSTGRES_PASS | base64 -d` \n"
+
+   echo -e "\n"
+
+}
+
+status()
+
+{
+   if [ $1 = "standby" ]; then
+
+      kubectl  logs acid-standby-cluster-0 | grep "FATAL:  password authentication failed" > /dev/null 
+      if [[ $? = 0  ]]; then 
+         secret_patch
+         echo "Problem with authetication"
+         echo "Deleting the pods on standby"
+         kubectl delete pod acid-standby-cluster-0 acid-standby-cluster-1
+         show_pass
+         
+      else 
+         echo "No problem with authentcation"
+         show_pass
+         exit
+      fi 
+
+   else 
+
+
+      kubectl get postgresqls.acid.zalan.do | grep Failed > /dev/null
+      if [ $? = 0 ]; then
+         echo -e "\n"
+         echo "failed  to provsion `kubectl get postgresqls.acid.zalan.do | grep Failed  | awk '{print $1}'` cluster"
+         echo -e "\n"
+         kubectl get postgresqls.acid.zalan.do
+      fi  
+
+   fi
+
+}
+
+
+
+echo "Installing Postgres Operator & CRD on main cluster.................................!"
+
 
 kubectl apply -k ./
 
+
+while [[ $(kubectl get pods -l name=postgres-operator -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+   wait 8
+   echo "Operator Pod is still creating......! wait for a while"
+done
+
+
 echo -e "\n"
 
-sleep 3 
+echo -e "Installing Postgres cluster........................................................!"
 
-kubectl wait   --for=condition=ready pod  -l name=postgres-operator --timeout=200s -n postgres
+kubectl apply -f postgres-operator/minimal-postgres-manifest-12.yaml
 
-kubectl wait   --for=condition=ready pod  -l cluster-name=acid-main-cluster   --timeout=200s -n postgres
 
-echo -e "Installing standby cluster\n"
+while [[ $(kubectl get postgresqls.acid.zalan.do acid-main-cluster -ojson | jq -r .status.PostgresClusterStatus) != "Running" ]]; do
+   wait 8
+   status main
+   echo "Postgres Pod is still creating......! wait for a while"
+done
 
+
+echo -e "\n"
+
+echo -e "Installing standby cluster.........................................................!"
 
 kubectl apply -f postgres-operator/standby-manifest.yaml
 
+while [[ $(kubectl get postgresqls.acid.zalan.do acid-standby-cluster -ojson | jq -r .status.PostgresClusterStatus) != "Running" ]]; do
+   wait 8
+   status standby
+   echo -e "\n" 
+   echo "Standby Pod is still creating......! wait for a while"
+done
 
-POSTGRES_PASS=`kubectl get secrets -n postgres postgres.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
-
-STANDBY_PASS=`kubectl get secrets -n postgres standby.acid-main-cluster.credentials.postgresql.acid.zalan.do  -ojson  | jq .data.password | sed -e 's/"//g'`
-
-
-# echo -e "standby password: `echo $STANDBY_PASS  | base64 -d`\n"
-
-
-
-echo -e "Patching the standby cluster with main cluster secret"
-
-
-kubectl patch secret postgres.acid-standby-cluster.credentials.postgresql.acid.zalan.do --type='json' -p='[{"op" : "replace" ,"path" : "/data/password" ,"value" : "'${POSTGRES_PASS}'"}]'
-
-
-echo -e "\n"
-
-
-echo -e "Patching the standby cluster with main cluster secret"
-
-kubectl patch secret standby.acid-standby-cluster.credentials.postgresql.acid.zalan.do --type='json' -p='[{"op" : "replace" ,"path" : "/data/password" ,"value" : "'${STANDBY_PASS}'"}]'
-
-
-
-kubectl  logs acid-standby-cluster-0 | grep "FATAL:  password authentication failed" > /dev/null
-
-if [ $? = 0 ]; then
-echo "Pod is not running need a restart"
-
-kubectl delete pod acid-standby-cluster-0
-
-
-else 
-echo "Successfully patched the secrete on standby cluster"
-fi 
-
-
-echo -e "postgresql password: `echo $POSTGRES_PASS | base64 -d` \n"
-
-echo -e "\n"
-
-# echo  -e "forwarding port to local \n "
-# pc
-
-# echo -e "ACID MAIN"
-# kubectl  port-forward acid-main-cluster-0 5434:5432    & 
-
-
-# echo -e "ACID STANDBY"
-# kubectl  port-forward acid-standby-cluster-0 5435:5432 
 
